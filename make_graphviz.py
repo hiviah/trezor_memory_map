f45cacb91f89839c46ad2e60acb490854b025473
Ondrej Mikle <ondrej.mikle@gmail.com>
2021-08-23 12:49:51 +0200

    Fix None children bug. Looking for better graph IDs.

--- make_graphviz.py
+++ make_graphviz.py
@@ -15,16 +15,30 @@ class Node:
         return "Node(%s, %d children)" % (self.object["ptr"], len(self.children))
 
     def __eq__(self, other):
-        if self.object["ptr"] == "(nil)":
+        if self.is_nil():
             return False # nil addr object we declare by definition never match
         return self.object["ptr"] == other.object["ptr"]
 
     def __hash__(self):
         # children can have nil address, but we don't want to clump them together in one hash bin
-        if self.object["ptr"] == "(nil)":
+        if self.is_nil():
             return hash(str(self.object))  # this is fucking stupid, but we don't have frozendict
         else:
             return hash(self.object["ptr"])
+
+    @property
+    def address(self):
+        return self.object["ptr"]
+
+    def is_nil(self):
+        return self.object["ptr"] == "(nil)"
+
+    @property
+    def graph_id(self):
+        if self.is_nil():
+            return str(self.object)
+        else:
+            return self.address
 
 
 def get_global_node(child):
@@ -80,7 +94,7 @@ for object in j:
             children = object.get("items")
             for child in children:
                 item_node = get_global_node(child)
-                if node is not None:
+                if item_node is not None:
                     child_nodes.add(item_node)
 
         node.children.update(child_nodes)
@@ -102,14 +116,15 @@ for addr, node in obj_map.items():
 dot_graph = AGraph(directed=True)
 
 some_nodes = [node for node in obj_map.values() if len(node.children) > 5]
+some_nodes = some_nodes[:20]
 for idx, node in enumerate(some_nodes):
-    dot_graph.add_node(node.object["ptr"])
-    for child in (child for child in node.children if child is not None and child.object["ptr"] != "(nil)"):
-        dot_graph.add_node(child.object["ptr"])
+    dot_graph.add_node(node.graph_id)
+    for child in node.children:
+        dot_graph.add_node(child.graph_id)
 
 for idx, node in enumerate(some_nodes):
-    for child in (child for child in node.children if child is not None and child.object["ptr"] != "(nil)"):
-        dot_graph.add_edge(node.object["ptr"], child.object["ptr"])
+    for child in node.children:
+        dot_graph.add_edge(node.graph_id, child.graph_id)
 
 dot_graph.write("tjost.dot")
 dot_graph.draw("tjost.png", prog="circo")
